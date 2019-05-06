54351ec776b650014d8dc0ed361c9a4b5379cd0b
Nuno Silva <nunosilva800@gmail.com>
2019-05-06 15:11:09 +0100

    3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,9 +8,24 @@ class Bottles
   private
 
   def sing(previous, current)
-    <<-VERSE
-#{previous} bottles of beer on the wall, #{previous} bottles of beer.
-Take one down and pass it around, #{current} bottles of beer on the wall.
-    VERSE
+    line1(previous) + "\n" + line2(current) + "\n"
+  end
+
+  def line1(num)
+    "#{pluralise('bottle', num)} of beer on the wall, "\
+      "#{pluralise('bottle', num)} of beer."
+  end
+
+  def line2(num)
+    "Take one down and pass it around, "\
+      "#{pluralise('bottle', num)} of beer on the wall."
+  end
+
+  def pluralise(string, count)
+    if count == 1
+      "#{count} #{string}"
+    else
+      "#{count} #{string}s"
+    end
   end
 end
